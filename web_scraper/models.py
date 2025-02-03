e67ce0a66c67711389704f4ccd9c0657f06f285d
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-02-03 19:29:54 -0300

    [ADD] remove imagem field from Produto model and create ImagemProduto model for image handling

--- web_scraper/models.py
+++ web_scraper/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
-    imagem = models.URLField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     preco_sem_desconto = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     percentual_desconto = models.FloatField(null=True, blank=True)
@@ -13,3 +12,11 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
+    
+class ImagemProduto(models.Model):
+    produto = models.ForeignKey(Produto, related_name="imagens", on_delete=models.CASCADE)
+    url_imagem = models.URLField()
+
+    def __str__(self):
+        return f"Imagem de {self.produto.nome}"
