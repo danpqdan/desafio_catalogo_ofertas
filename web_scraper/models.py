57a61cf6384da8f43618379b33ee8ffca6f4923e
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-02-03 19:25:14 -0300

    [ADD] create initial migration and model for Produto

--- web_scraper/models.py
+++ web_scraper/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    imagem = models.URLField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_sem_desconto = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    percentual_desconto = models.FloatField(null=True, blank=True)
+    parcelamento = models.CharField(max_length=100, null=True, blank=True)
+    link = models.URLField()
+    tipo_entrega = models.CharField(max_length=50, null=True, blank=True)
+    frete_gratis = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
